14f0d1da28e958f75af3aa2c193819f2e85dfadc
Steve de Peijper <steve.depeijper@gmail.com>
2023-01-10 08:37:46 +0100

    improvements

--- context.vb
+++ context.vb
@@ -1,50 +1,44 @@
-Sub loop_excel()
+Sub create_callouts()
  'load excel
      Dim app As New Excel.Application
      app.Visible = False 'Visible is False by default, so this isn't necessary
      Dim book As Excel.Workbook
      Set book = app.Workbooks.Add(FileOpenDialogBox)
     
-
     Dim planningslidenr As Integer
     planningslidenr = InputBox("Which Slide contains the planning")
+    
+    'retreive the first table
     Set lstActivities = book.Worksheets("Sheet1").ListObjects(1)
+    
+
+    'for each task
         For i = 1 To lstActivities.ListRows.Count
-            'If lstActivities.ListColumns("ID").DataBodyRange(i).Value = "1" Then
-                'MsgBox lstActivities.ListColumns("Name").DataBodyRange(i).Value
-               
-               If Len(LTrim(RTrim(lstActivities.ListColumns("External ID").DataBodyRange(i).Value))) > 0 Then
-                   
-                   If context_new(lstActivities.ListColumns("External ID").DataBodyRange(i).Value, planningslidenr) = 1 Then
-                    
+                'if the task ID is greater than 0
+               If Len(LTrim(RTrim(lstActivities.ListColumns("ID").DataBodyRange(i).Value))) > 0 Then
+                   ' start creating a new fn_new_callout, navigate to the function to learn more.
+                   If fn_search_bar_and_create_callout(lstActivities.ListColumns("ID").DataBodyRange(i).Value, planningslidenr) = 1 Then
+                    'once the new slide with the shapes has been created we populate the last shape (text box with text)
                     Set currentslide = ActivePresentation.Slides(ActiveWindow.View.Slide.SlideIndex)
                     Set tr = currentslide.Shapes(currentslide.Shapes.Count).TextFrame.TextRange
                     tr.Font.Size = 12
+                    'We populate the textbox over here, for some reason sorting/filtering needed to be disables for this to work properly
                     tr.Text = lstActivities.ListColumns("Title").DataBodyRange(i).Value & _
                     vbNewLine & vbNewLine & "Objective" & vbNewLine & _
-                    lstActivities.ListColumns("Description").DataBodyRange(i).Value & _
+                    lstActivities.ListColumns("Objective").DataBodyRange(i).Value & _
                     vbNewLine & vbNewLine & "Approach" & vbNewLine & _
-                    lstActivities.ListColumns("Generic03").DataBodyRange(i).Value & _
-                    vbNewLine & vbNewLine & "Input" & vbNewLine & _
-                    lstActivities.ListColumns("Generic02").DataBodyRange(i).Value & _
-                    vbNewLine & vbNewLine & "Output / Deliverable" & vbNewLine & _
-                    lstActivities.ListColumns("Acceptance Criteria").DataBodyRange(i).Value & _
+                    lstActivities.ListColumns("Approach").DataBodyRange(i).Value & _
                     vbNewLine & vbNewLine & "Responsible" & vbNewLine & _
-                    lstActivities.ListColumns("Assigned To").DataBodyRange(i).Value & _
-                    vbNewLine & vbNewLine & "Timing" & vbNewLine & _
-                    lstActivities.ListColumns("Iteration Path").DataBodyRange(i).Value '& _
-                   ' vbNewLine & vbNewLine & "Status" & vbNewLine & _
-                   ' lstActivities.ListColumns("Status").DataBodyRange(i).Value
+                    lstActivities.ListColumns("Responsible").DataBodyRange(i).Value
     
-    Debug.Print "id:"
-    Debug.Print lstActivities.ListColumns("External ID").DataBodyRange(i).Value & vbNewLine
-    
+                    'set some formatting.
                     tr.Paragraphs(1).Font.Bold = msoTrue
                     tr.Paragraphs(1).Font.Size = 18
                     tr.Paragraphs(1).Font.Color = RGB(0, 137, 196)
                     
+                   'We want to make certain fields bold.
                   Dim arr As Variant
-                    arr = Array("Objective", "Approach", "Input", "Output / Deliverable", "Responsible", "Timing")
+                    arr = Array("Objective", "Approach", "Responsible")
                   For Each word In arr
                     Set foundText = tr.Find(FindWhat:=word)
                     Do While Not (foundText Is Nothing)
@@ -56,19 +50,9 @@ Sub loop_excel()
                         End With
                     Loop
                 Next
-                    
-                    'tr.Paragraphs(3).Font.Bold = msoTrue
-                    'tr.Paragraphs(6).Font.Bold = msoTrue
-                    'tr.Paragraphs(9).Font.Bold = msoTrue
-                    'tr.Paragraphs(11).Font.Bold = msoTrue
-                    'tr.Paragraphs(15).Font.Bold = msoTrue
-                    'tr.Paragraphs(18).Font.Bold = msoTrue
-                'tr.Paragraphs(21).Font.Bold = msoTrue
-                
-                ' tr.Lines(5).Font.Bold = msoTrue
                End If
             End If
-            'End If
+  
         Next i
                     
 'close off
@@ -79,53 +63,42 @@ Sub loop_excel()
     Set currentslide = Nothing
     Set tr = Nothing
     
-    'MsgBox "done"
 
 End Sub
 
-Function context_new(id As String, planningslidenr As Integer) As Integer
-'select slide
-context_new = 0
-    
-    Set shps = ActivePresentation.Slides(CInt(planningslidenr)).Shapes
+Function fn_search_bar_and_create_callout(id As String, planningslidenr As Integer) As Integer
 
+fn_search_bar_and_create_callout = 0
+    Set shps = ActivePresentation.Slides(CInt(planningslidenr)).Shapes
     Dim a As Integer
 
     For a = 1 To shps.Count
             If shps(a).Name = id Then
-                context CInt(planningslidenr), a
-                context_new = 1
+                fn_new_callout CInt(planningslidenr), a
+                fn_search_bar_and_create_callout = 1
             End If
     Next a
-    
-   
 
 End Function
 
 
-Sub context(slidenr As Integer, shapenr As Integer)
-
-   
-    
+Sub fn_new_callout(slidenr As Integer, shapenr As Integer)
     Dim sld As Slide
     Dim shp As Shape
     
-    Set sld = ActivePresentation.Slides(slidenr) 'Application.ActiveWindow.View.Slide
+    Set sld = ActivePresentation.Slides(slidenr)
     Set shp = sld.Shapes(shapenr)
     
     sld.Duplicate
-    
       
-    y = shp.Top ' ActiveWindow.Selection.ShapeRange.Top
-    x = shp.Left 'ActiveWindow.Selection.ShapeRange.Left
-    w = shp.Width 'ActiveWindow.Selection.ShapeRange.Width
-    h = shp.Height 'ActiveWindow.Selection.ShapeRange.Height
-    
+    y = shp.Top
+    x = shp.Left
+    w = shp.Width
+    h = shp.Height
    
-    shp.Copy 'Windows(1).Selection.Copy
-  '  MsgBox sld.Shapes.Count
+    'shp.Copy
     Set sld = ActivePresentation.Slides(slidenr + 1)
-    sld.Shapes.Paste
+    'sld.Shapes.Paste
      sld.Shapes(sld.Shapes.Count).Top = y
      sld.Shapes(sld.Shapes.Count).Left = x
      
@@ -136,22 +109,20 @@ Sub context(slidenr As Integer, shapenr As Integer)
      sld.Shapes(sld.Shapes.Count).Fill.Transparency = 0.35
      ActiveWindow.View.GotoSlide slidenr + 1
     
-    sld.Shapes.Range(Array(sld.Shapes.Count, n)).Select
-    ActiveWindow.Selection.ShapeRange.MergeShapes msoMergeCombine
+    sld.Shapes(shapenr).ZOrder (msoBringToFront)
+    
+    'sld.Shapes.Range(Array(sld.Shapes.Count, n)).Select
+    'ActiveWindow.Selection.ShapeRange.MergeShapes msoMergeCombine
         
-    
-    
-    
     Set shp = Nothing
+    
     Dim ffb As FreeformBuilder
     Dim myshape As Shape
     Dim currentslide As Slide
     
-    Set currentslide = sld 'ActivePresentation.Slides(ActiveWindow.View.Slide.SlideIndex)
-
-    'Links = MsgBox("Left?", vbYesNo + vbQuestion, "Side")
+    Set currentslide = sld
     
-    If x > 600 Then
+    If x > Application.ActivePresentation.PageSetup.SlideWidth * 0.4 Then
            Set ffb = currentslide.Shapes.BuildFreeform(msoEditingCorner, x, y)
             With ffb
                 .AddNodes msoSegmentLine, msoEditingAuto, x - 40, 135
@@ -181,23 +152,11 @@ Sub context(slidenr As Integer, shapenr As Integer)
            currentslide.Shapes.AddTextbox(msoTextOrientationHorizontal, _
                 Left:=x + w + 40, Top:=30, Width:=300, Height:=500).TextFrame _
                 .TextRange.Text = ""
-
-        
-    
-    
     End If
     
     Set myshape = ffb.ConvertToShape
     myshape.Fill.ForeColor.RGB = RGB(256, 256, 256)
-    
-    'MsgBox currentslide.Shapes(currentslide.Shapes.Count).Name
     currentslide.Shapes(currentslide.Shapes.Count).ZOrder (msoSendBackward)
-   ' MsgBox currentslide.Shapes(currentslide.Shapes.Count).TextFrame.TextRange.Lines.Count
-    
-    'Set tr = currentslide.Shapes(currentslide.Shapes.Count).TextFrame.TextRange
-    
-    'tr.Lines(2, 2).Font.Italic = msoTrue
-    
     
     Set tb = Nothing
     Set sld = Nothing
@@ -205,9 +164,6 @@ Sub context(slidenr As Integer, shapenr As Integer)
     Set currentslide = Nothing
 
 End Sub
-
-
-
 
 Function FileOpenDialogBox() As String
  
@@ -227,6 +183,9 @@ Function FileOpenDialogBox() As String
     End With
  
 End Function
+
+
+
 
 
 
